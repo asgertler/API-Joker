eae23b1db657f82e000d0a9ee84db66f01bc29f8
Aaron Gertler <aaron.gertler@gmail.com>
2020-08-28 11:43:04 -0500

    successfully pull jokes from API and log them with button click

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,14 @@
+import { getJoke, useJoke } from './JokeProvider.js'
+
+var joke = []
+
 const jokeBtn = document.querySelector(".request-joke-btn");
 
-jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...");
+jokeBtn.addEventListener("click", () => {
+  getJoke()
+    .then(_ => {
+      joke = useJoke()
+      console.log(joke)
+    })
   // TODO: fetch a joke from the Joke API and render it to the DOM
 });
